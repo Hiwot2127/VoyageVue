c46a1eac606db2c8c7ac18464a2beb97610d7fa7
demisu-s <damisusefu@gmail.com>
2023-09-05 11:51:22 +0300

    update

--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken'); // Add JWT library
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
-
-
-
-
 // MongoDB Configuration (change the connection string as needed)
-
-
 mongoose.connect('mongodb+srv://root1:root12@traficcongest.qfouane.mongodb.net/users?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -28,7 +21,6 @@ mongoose.connection.on('error', (err) => {
   console.error('MongoDB connection error:', err);
 });
 
-
 // Define User model
 const User = mongoose.model('User', {
   firstName: String,
@@ -37,6 +29,7 @@ const User = mongoose.model('User', {
   password: String,
 });
 
+app.use(cors());
 app.use(bodyParser.json());
 
 // Registration endpoint
@@ -62,46 +55,18 @@ app.post('/api/register', async (req, res) => {
 
     await newUser.save();
 
-    // Generate JWT token for authentication (if needed)
-    const token = jwt.sign({ userId: newUser._id }, 'your-secret-key', { expiresIn: '1h' });
-
-    return res.status(201).json({ message: 'Registration successful', token });
+    return res.status(201).json({ message: 'Registration successful' });
   } catch (error) {
     console.error('Registration failed:', error);
     return res.status(500).json({ message: 'Registration failed.' });
   }
 });
 
-// Login endpoint
-app.post('/api/login', async (req, res) => {
-  try {
-    const { email, password } = req.body;
+// Login endpoint (you can implement this separately)
 
-    // Find the user by email
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(404).json({ message: 'User not found.' });
-    }
-
-    // Compare the provided password with the stored hashed password
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-    if (!isPasswordValid) {
-      return res.status(401).json({ message: 'Invalid password.' });
-    }
-
-    // Generate JWT token for authentication (if needed)
-    const token = jwt.sign({ userId: user._id }, 'your-secret-key', { expiresIn: '1h' });
-
-    return res.status(200).json({ message: 'Login successful', token });
-  } catch (error) {
-    console.error('Login failed:', error);
-    return res.status(500).json({ message: 'Login failed.' });
-  }
-});
 app.get('/', (req, res) => {
-    res.send("Here the server is waiting for you");
-  });
-  
+  res.send('Server is running and waiting for requests.');
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -9,6 +9,7 @@ export default class SignUp extends Component {
       lastName: '',
       email: '',
       password: '',
+      error: '',
     };
   }
 
@@ -16,81 +17,85 @@ export default class SignUp extends Component {
     this.setState({ [e.target.name]: e.target.value });
   };
 
-  handleSubmit = (e) => {
+  handleSubmit = async (e) => {
     e.preventDefault();
     const { firstName, lastName, email, password } = this.state;
 
-    // Make a POST request to your backend for registration
-    axios
-      .post('/api/register', { firstName, lastName, email, password })
-      .then((response) => {
-        // Handle successful registration
-        console.log(response.data.message);
-        // Redirect or set authentication state as needed
-      })
-      .catch((error) => {
-        // Handle registration error
-        console.error('Registration failed:', error);
+    try {
+      const response = await axios.post('http://localhost:3001/api/register', {
+        firstName,
+        lastName,
+        email,
+        password,
       });
+
+      // Handle successful registration
+      console.log(response.data.message);
+      // Redirect or set authentication state as needed
+    } catch (error) {
+      // Handle registration error
+      console.error('Registration failed:', error.response.data.message);
+      this.setState({ error: error.response.data.message || 'Registration failed.' });
+    }
   };
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <div>
         <h3>Sign Up</h3>
-        <div className="mb-3">
-          <label>First name</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="First name"
-            name="firstName"
-            value={this.state.firstName}
-            onChange={this.handleInputChange}
-          />
-        </div>
-        <div className="mb-3">
-          <label>Last name</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Last name"
-            name="lastName"
-            value={this.state.lastName}
-            onChange={this.handleInputChange}
-          />
-        </div>
-        <div className="mb-3">
-          <label>Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Enter email"
-            name="email"
-            value={this.state.email}
-            onChange={this.handleInputChange}
-          />
-        </div>
-        <div className="mb-3">
-          <label>Password</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Enter password"
-            name="password"
-            value={this.state.password}
-            onChange={this.handleInputChange}
-          />
-        </div>
-        <div className="d-grid">
-          <button type="submit" className="btn btn-primary">
-            Sign Up
-          </button> 
-        </div>
-        <p className="forgot-password text-right">
-          Already registered <a href="/sign-in">sign in?</a>
-        </p>
-      </form>
+        {this.state.error && <p className="error-message">{this.state.error}</p>}
+        <form onSubmit={this.handleSubmit}>
+          <div className="mb-3">
+            <label>First name</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="First name"
+              name="firstName"
+              value={this.state.firstName}
+              onChange={this.handleInputChange}
+            />
+          </div>
+          <div className="mb-3">
+            <label>Last name</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Last name"
+              name="lastName"
+              value={this.state.lastName}
+              onChange={this.handleInputChange}
+            />
+          </div>
+          <div className="mb-3">
+            <label>Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Enter email"
+              name="email"
+              value={this.state.email}
+              onChange={this.handleInputChange}
+            />
+          </div>
+          <div className="mb-3">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Enter password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleInputChange}
+            />
+          </div>
+          <div className="d-grid">
+            <button type="submit" className="btn btn-primary">
+              Sign Up
+            </button>
+          </div>
+        </form>
+      </div>
     );
   }
 }
